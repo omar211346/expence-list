831569bf6037fbfd081c2be9a6f3b914613305bc
omar <omarfarah2002@hotmail.com>
2025-05-09 16:21:39 +0200

    added expenceform with handlesubmit

--- src/components/ExpenceForm.jsx
+++ src/components/ExpenceForm.jsx
@@ -1,1 +1,14 @@
 import React from "react";
+
+const ExpenseForm = ({ addExpense }) => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newExpense = {
+      id: Date.now(),
+      title: e.target.title,  
+      amount: e.target.amount, 
+      date: e.target.date,    
+      category: e.target.category, 
+    };
+    addExpense(newExpense);
+  }};
