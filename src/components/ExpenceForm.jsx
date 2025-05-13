42146d50882862992d8f346afa128e5fd07bd891
omar <omarfarah2002@hotmail.com>
2025-05-13 13:38:20 +0200

    finished the input for handling

--- src/components/ExpenceForm.jsx
+++ src/components/ExpenceForm.jsx
@@ -5,10 +5,29 @@ const ExpenseForm = ({ addExpense }) => {
     e.preventDefault();
     const newExpense = {
       id: Date.now(),
-      title: e.target.title,  
+      title: e.target.title, 
       amount: e.target.amount, 
-      date: e.target.date,    
+      date: e.target.date,   
       category: e.target.category, 
     };
+    console.log("New Expense:", newExpense);
     addExpense(newExpense);
-  }};
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" placeholder="Title" name="title" />
+      <input type="number" placeholder="Amount" name="amount" />
+      <input type="date" name="date" />
+      <select name="category">
+        <option value="">Select Category</option>
+        <option value="housing">Housing</option>
+        <option value="utilities">Utilities</option>
+        <option value="grocery">Grocery</option>
+      </select>
+      <button type="submit">Add Expense</button>
+    </form>
+  );
+};
+
+export default ExpenseForm;
