62a5c10d60c7732edd02a53c34b4055f43657bf1
omar <omarfarah2002@hotmail.com>
2025-05-14 12:30:14 +0200

    passed the delete prop from list to item

--- src/components/ExpenseList.jsx
+++ src/components/ExpenseList.jsx
@@ -1,9 +1,7 @@
 import React from "react";
 import ExpenseItem from "./ExpenseItem";
 
-const ExpenseList = ({ expenses }) => {
-
-
+const ExpenseList = ({ expenses, deleteExpense }) => {
   return (
     <div>
       <h2>Number of expenses: {expenses.length}</h2>
@@ -11,7 +9,11 @@ const ExpenseList = ({ expenses }) => {
         <p>No expenses found</p>
       ) : (
         expenses.map((expense) => (
-          <ExpenseItem key={expense.id} expense={expense} />
+          <ExpenseItem 
+            key={expense.id} 
+            expense={expense} 
+            deleteExpense={deleteExpense} 
+          />
         ))
       )}
     </div>
