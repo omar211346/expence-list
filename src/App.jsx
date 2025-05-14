6a1a4d2b5b24f993ffdd88588bf55fbd0da9f682
omar <omarfarah2002@hotmail.com>
2025-05-14 16:01:07 +0200

    remove unnecessary ExpenseItem component

--- src/App.jsx
+++ src/App.jsx
@@ -10,39 +10,24 @@ function App() {
     { id: 3, title: "Internet", amount: 30, date: "2025-05-03", category: "Utilities" },
   ]);
 
- 
+
   const addExpense = (newExpense) => {
     setExpenses((prevExpenses) => [...prevExpenses, newExpense]);
   };
 
-  
+
   const deleteExpense = (id) => {
     const updatedExpenses = expenses.filter((expense) => expense.id !== id);
     setExpenses(updatedExpenses);
     console.log("Deleted expense with ID:", id);
   };
 
-  const ExpenseItem = ({ expense, deleteExpense }) => {
-    const handleDelete = () => {
-      console.log("Deleting expense with ID:", expense.id);
-      deleteExpense(expense.id);
-    };
-    return (
-      <div>
-        <h1>Expense Tracker</h1>
-        
-        <ExpenseForm addExpense={addExpense} />
-        <ExpenseList expenses={expenses} deleteExpense={deleteExpense} />
-      </div>
-    );
-  }
   return (
     <div>
       <h1>Expense Tracker</h1>
-    
-      <ExpenseForm addExpense={addExpense} />
       
      
+      <ExpenseForm addExpense={addExpense} />
       <ExpenseList expenses={expenses} deleteExpense={deleteExpense} />
     </div>
   );
