eb8d63f686249d4dd9cbb429f24779f7920ed840
omar <omarfarah2002@hotmail.com>
2025-05-15 13:23:40 +0200

    forgot to commit

--- src/components/EditForm.jsx
+++ src/components/EditForm.jsx
@@ -1,6 +1,26 @@
 import React from "react";
 
-const EditForm = ({ expense }) => {
+    const EditForm = ({ expense }) => {
+        
+        const [title, setTitle] = useState(expense.title);
+        const [amount, setAmount] = useState(""); 
+        const [date, setDate] = useState(expense.date);
+        const [category, setCategory] = useState(expense.category);
+      
+       
+        const handleTitleChange = (e) => {
+          setTitle(e.target); // 
+        };
+      
+        
+        const handleAmountChange = (e) => {
+          setAmount(e.target.value);
+        };
+      
+        const handleCategoryChange = (e) => {
+          setCategory(e.target.value);
+        };
+      
   return (
     <form>
       <input type="text" placeholder="Title" value={expense.title} />
